332fa6d12ba6b41e914aeb462c133fd94f28c9c8
milansegedinac <milan.segedinac@gmail.com>
2016-01-01 13:39:22 +0100

    loops

---
+++ primeri/python/prosek.py
@@ -0,0 +1,6 @@
+n=input("unesi n: ")
+suma=0
+for i in range(1,n+1):
+	suma = suma+i
+prosek=float(suma)/n #zasto sumu moramo da pretvorimo u float?
+print "prosek=",prosek
---
+++ primeri/python/suma.py
@@ -0,0 +1,6 @@
+suma = 0
+broj = raw_input("unesi broj: ")
+while broj!="":
+	suma = suma + float(broj)
+	broj = raw_input("unesi broj: ")
+print "suma unetih brojeva je: ", suma
